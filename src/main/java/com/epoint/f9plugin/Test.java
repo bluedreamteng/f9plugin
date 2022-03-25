0c98fb172b03063d61a7453221357474151568f0
ztf <ztf>
2022-03-25 10:04:17 +0800

    初始化插件工程

---
+++ src/main/java/com/epoint/f9plugin/Test.java
@@ -0,0 +1,7 @@
+package com.epoint.f9plugin;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/epoint/f9plugin/TestAction.java
@@ -0,0 +1,13 @@
+package com.epoint.f9plugin;
+
+import cn.hutool.Hutool;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class TestAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
